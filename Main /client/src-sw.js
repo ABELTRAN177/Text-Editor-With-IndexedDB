671b4969d487b06ae55197f5db1e6f46f1a02bd0
Angel B <angelb@Angels-MacBook-Pro.local>
2024-10-01 17:13:09 -0700

    updated main

--- Main /client/src-sw.js
+++ Main /client/src-sw.js
@@ -5,39 +5,37 @@ const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
 
-
 precacheAndRoute(self.__WB_MANIFEST);
 
+// Set up page cache
 const pageCache = new CacheFirst({
-    cacheName: 'page-cache',
-    plugins: [
-        new CacheableResponsePlugin({
-            statuses: [0, 200],
-        }),
-        new ExpirationPlugin({
-            maxEntries: 50,
-            maxAgeSeconds: 30 * 24 * 60 * 60,
-        }),
-    ],
+  cacheName: 'page-cache',
+  plugins: [
+    new CacheableResponsePlugin({
+      statuses: [0, 200],
+    }),
+    new ExpirationPlugin({
+      maxAgeSeconds: 30 * 24 * 60 * 60,
+    }),
+  ],
 });
 
 warmStrategyCache({
-    urls: [
-        '/',
-        '/index.html'
-    ], strategy: pageCache,
+  urls: ['/index.html', '/'],
+  strategy: pageCache,
 });
- 
+
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
+// Set up asset cache
 registerRoute(
-    ({ request}) => ['style', 'script', 'worker'].includes(request.destination),
-    new StaleWhileRevalidate({
-        cacheName: 'assets-cache',
-        plugins: [
-            new CacheableResponsePlugin({
-                statuses: [0, 200],
-            }),
-        ],
-    })
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    cacheName: 'asset-cache',
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
 );
--- Main /client/src/js/install.js
+++ Main /client/src/js/install.js
@@ -1,21 +1,24 @@
-const butInstall = document.getElementById('butInstall');
+const butInstall = document.getElementById('buttonInstall');
 
 window.addEventListener('beforeinstallprompt', (event) => {
-    window.deferredPrompt = event;
-    butInstall.removeAttribute('hidden', false);
+  window.deferredPrompt = event;
+  butInstall.classList.toggle('hidden', false);
 });
 
 butInstall.addEventListener('click', async () => {
-    const promptEvent = window.deferredPrompt;
-    if (!promptEvent) {
-        return;
-    }
-    promptEvent.prompt();
-    
-    window.deferredPrompt = null;
-    butInstall.classList.toggle('hidden', true);
+  const promptEvent = window.deferredPrompt;
+
+  if (!promptEvent) {
+    return;
+  }
+
+  promptEvent.prompt();
+
+  window.deferredPrompt = null;
+
+  butInstall.classList.toggle('hidden', true);
 });
 
 window.addEventListener('appinstalled', (event) => {
-    window.deferredPrompt = null;
+  window.deferredPrompt = null;
 });
